b6c69eccc7290a9f164592bf3e592bda617e806c
ahacad <ahacad@126.com>
2023-11-13 13:19:29 -0800

    chore: remove curl nvim local share since using lazyvim now

--- src/downloads.sh
+++ src/downloads.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/Ahacad/wallpaper "$HOME"
 
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
