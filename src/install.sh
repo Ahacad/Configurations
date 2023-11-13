cf7e76539a121be54f05f6d125897a5874bf06f5
ahacad <ahacad@126.com>
2023-11-13 12:28:29 -0800

    feat: update install.sh
    - add install and backup functions
    - use command line arguments to select between install/backup
    - CONFIG_BACKUP_DIR as the environmental control for config backup location, default to ../configs which means you are running the backup script from the current source code directory

--- src/install.sh
+++ src/install.sh
@@ -1,23 +1,104 @@
 #! /usr/bin/sh
 
 install_config() {
-    # rm pcfolder/configfile
-    rm -rf "$1/$3"
-    # cp configfolder/configfile pcfolder
-    cp -r "$2/$3" "$1"
+    local local_dir=$1
+    local backup_dir=$2
+    local filename=$3
+    local verbose=${4:-true}
+    
+    local local_path="$local_dir"
+    local backup_path="$backup_dir/$filename"
+
+    [ "$verbose" = true ] && echo "Installing $filename to $local_dir"
+    
+    # check if local config file exists
+    if [ -e "$local_path/$filename" ]; then
+        [ "$verbose" = true ] && echo "Removing existing file: $local_path/$filename"
+        rm -rf "$local_path/$filename"
+    fi
+
+    # copy file and check for success
+    cp -r "$backup_path" "$local_path" && {
+        [ "$verbose" = true ] && echo "Successfully installed $filename"
+    } || {
+        echo "Failed to install $filename" >&2
+        return 1
+    }
 }
 
-install_config "$HOME/.config" "../configs" "nvim"
-install_config "$HOME/.config" "../configs" "sxhkd"
-install_config "$HOME/.config" "../configs" "newsboat"
-install_config "$HOME/.config" "../configs" "alacritty"
-install_config "$HOME/.config" "../configs" "bspwm"
-install_config "$HOME/.config" "../configs" "polybar"
-install_config "$HOME/.config" "../configs" "SELFMADE"
-install_config "$HOME/.config" "../configs" "lf"
-install_config "$HOME/.config" "../configs" "redshift"
-install_config "$HOME/.config/ibus" "../configs" "rime"
-install_config "$HOME" "../configs" ".tmux.conf"
-install_config "$HOME" "../configs" ".gitconfig"
-install_config "$HOME" "../configs" ".zshrc"
-install_config "$HOME" "../configs" ".oh-my-zsh"
+backup_config() {
+    local local_dir=$1
+    local backup_dir=$2
+    local filename=$3
+    local verbose=${4:-true}
+
+    local local_path="$local_dir/$filename"
+    local backup_path="$backup_dir"
+
+    [ "$verbose" = true ] && echo "Backing up $filename to $dest_dir"
+
+    # check if local config file exists
+    if [ -e "$local_path" ]; then
+        mkdir -p "$backup_dir"
+
+        # copy file and check for success
+        cp -r "$local_path" "$backup_path" && {
+            [ "$verbose" = true ] && echo "Successfully backed up $filename"
+        } || {
+            echo "Failed to back up $filename" >&2
+            return 1
+        } 
+    else
+        [ "$verbose" = true ] && echo "No existing file: $local_path"
+    fi
+}
+
+declare -a configs=(
+    ".config/nvim"
+    ".config/sxhkd"
+    ".config/newsboat"
+    ".config/alacritty"
+    ".config/bspwm"
+    ".config/polybar"
+    ".config/SELFMADE"
+    ".config/lf"
+    ".config/redshift"
+    ".config/ibus/rime"
+    ".tmux.conf"
+    ".gitconfig"
+    ".zshrc"
+    ".oh-my-zsh"
+)
+
+for config in "${configs[@]}"; do
+    directory=$(dirname "$config")
+    filename=$(basename "$config")
+
+    [ "$directory" = "." ] && directory=""
+
+    install_config "$HOME/$directory" "../configs" "$filename" true
+done
+
+case "$1" in
+    install)
+        for config in "${configs[@]}"; do
+            directory=$(dirname "$config")
+            filename=$(basename "$config")
+            [ "$directory" = "." ] && directory=""
+            install_config "$HOME/$directory" "../configs" "$filename" true
+        done
+        ;
+    backup)
+        backup_dir="${CONFIG_BACKUP_DIR:-../configs}"
+        echo "Backing up configs to $backup_dir"
+        for config in "${configs[@]}"; do
+            directory=$(dirname "$config")
+            filename=$(basename "$config")
+            [ "$directory" = "." ] && directory=""
+            backup_config "$HOME/$directory" "$backup_dir" "$filename" true
+        ;;
+    *)
+        echo "Usage: $0 {install|backup}"
+        exit 1
+        ;;
+esac
