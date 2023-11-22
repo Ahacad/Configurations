7a69957a26306414f67484809162af540ce86930
ahacad <ahacad@126.com>
2023-11-21 17:47:02 -0800

    chore: SELFMADE alias and nvim keymap update
    SELFMADE alias: no longer mount non-exist disk, no longer start docker
    nvim keymap: add keymaps for Noice starting with <leader>n, ref: https://github.com/folke/noice.nvim?tab=readme-ov-file#-usage

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -15,4 +15,11 @@ map("x", "K", "5k", { desc = "move 5 lines down" })
 --- Q equals quit file, quit faster
 map("n", "Q", ":q<CR>", { desc = "quit file" })
 
+--- F equals save without autoformatting, etc.
 map("n", "F", ":noa w<CR>")
+
+map("n", "<leader>nd", "<cmd>Noice disable<cr>", { desc = "Disable Noice" })
+map("n", "<leader>ne", "<cmd>Noice enable<cr>", { desc = "Enable Noice" })
+map("n", "<leader>nl", "<cmd>Noice last<cr>", { desc="Get last Noice message" })
+map("n", "<leader>nh", "<cmd>Noice history", { desc = "Get Noice history" })
+map("n", "<leader>ni", "<cmd>Noice dismiss<cr>", { desc = "Dismiss Noice messages" })
