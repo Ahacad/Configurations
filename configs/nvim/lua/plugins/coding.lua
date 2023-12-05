de8aae6f072a14054c1483e8b3920d642ff5bb63
ahacad <rjiejin@gmail.com>
2023-12-05 07:20:36 -0800

    chore(nvim): add obsession.vim

--- configs/nvim/lua/plugins/coding.lua
+++ configs/nvim/lua/plugins/coding.lua
@@ -1,20 +1,29 @@
 return {
+  -- use ctrl + a/d to incresa/decrease number
   {
     "triglav/vim-visual-increment",
   },
+  -- generate toc for markdown
   {
     "mzlogin/vim-markdown-toc",
   },
+  -- multiple cursor simultaneously
   {
     "terryma/vim-multiple-cursors",
   },
   {
     "pechorin/any-jump.vim",
   },
+  -- edit surroundings quickly
   {
     "kylechui/nvim-surround",
   },
+  -- function, class names lists, etc.
   {
     "liuchengxu/vista.vim",
+  },
+  -- session management
+  {
+    "tpope/vim-obsession",
   },
 }
